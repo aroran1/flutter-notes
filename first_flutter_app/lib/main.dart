26ad80dfb9f8b1c4ffc175cae4ee61940ec62cb2
aroran1 <nidhigulati2001@gmail.com>
2021-04-22 18:13:35 +0100

    added questions list end with if/else

--- first_flutter_app/lib/main.dart
+++ first_flutter_app/lib/main.dart
@@ -18,51 +18,60 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  // creating a map
+  static const questions = [
+    {
+      'questionText': 'What\'s your favourite color?',
+      'answers': ['Red', 'Yellow', 'White', 'Black']
+    },
+    {
+      'questionText': 'What\'s your favourite animal?',
+      'answers': ['Rabbit', 'Dog', 'Elephant', 'Tiger']
+    },
+    {
+      'questionText': 'What\'s your favourite tutor?',
+      'answers': ['Max', 'Andrew', 'Dimitri', 'Chris']
+    }
+  ];
   var _questionIndex = 0;
 
   void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
-    print(_questionIndex);
+    // print(_questionIndex);
+    if (_questionIndex < questions.length) {
+      print('No more questions!');
+    } else {
+      print(_questionIndex);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // creating a map
-    const questions = [
-      {
-        'questionText': 'What\'s your favourite color?',
-        'answers': ['Red', 'Yellow', 'White', 'Black']
-      },
-      {
-        'questionText': 'What\'s your favourite animal?',
-        'answers': ['Rabbit', 'Dog', 'Elephant', 'Tiger']
-      },
-      {
-        'questionText': 'What\'s your favourite tutor?',
-        'answers': ['Max', 'Andrew', 'Dimitri', 'Chris']
-      }
-    ];
     // return MaterialApp(home: Text('Hello!'));
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App\'s title'),
         ),
-        body: Column(
-          children: [
-            Question(questions[_questionIndex]['questionText']),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                // Note: map doesn't alter the original questions list instead to makes copy of it and retuns a new list
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList()
-            // Answer(_answerQuestion),
-            // Answer(_answerQuestion),
-            // Answer(_answerQuestion),
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(questions[_questionIndex]['questionText']),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      // Note: map doesn't alter the original questions list instead to makes copy of it and retuns a new list
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                  // Answer(_answerQuestion),
+                  // Answer(_answerQuestion),
+                  // Answer(_answerQuestion),
+                ],
+              )
+            : Center(
+                child: Text('You did it!'),
+              ),
       ),
     );
   }
